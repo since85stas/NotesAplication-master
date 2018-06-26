995f831f3ba1cb6d0531c4b995033310d0bfd294
seeyou85stas <seeyou85@gmail.com>
2018-06-26 13:57:19 +0300

    Настроена работа поиска по заметкам

--- app/src/main/java/com/batura/stas/notesaplication/MainActivity.java
+++ app/src/main/java/com/batura/stas/notesaplication/MainActivity.java
@@ -2,7 +2,6 @@ package com.batura.stas.notesaplication;
 
 import android.app.LoaderManager;
 import android.app.SearchManager;
-import android.app.SearchableInfo;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -18,6 +17,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.SearchEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,7 +28,6 @@ import android.widget.SearchView;
 import com.batura.stas.notesaplication.data.NoteContract;
 import com.batura.stas.notesaplication.data.NoteDbHelper;
 
-import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>  {
@@ -39,7 +38,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private SearchView mSearchView;
 
+    private String mSearchString = null; // search request
+
     private static final int NOTE_LOADER = 0;
+
+    @Override
+    public boolean onSearchRequested(SearchEvent searchEvent) {
+
+        return super.onSearchRequested(searchEvent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,13 +88,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
-        getLoaderManager().initLoader(NOTE_LOADER,null,this);
-
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            //doMySearch(query);
+            mSearchString = intent.getStringExtra(SearchManager.QUERY);
+            //System.out.println(query);
         }
+        getLoaderManager().initLoader(NOTE_LOADER,null,this);
 
 
 
@@ -106,17 +112,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mSearchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
 
         setupSearchResult(searchMenuItem);
-//        searchView.setSearchableInfo(
-//                searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
 
     private void setupSearchResult (MenuItem searchItem ) {
         SearchManager searchManager =
                 (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        if (searchManager != null) {
-            List<SearchableInfo> searchables = searchManager.getSearchablesInGlobalSearch();
-        }
+        mSearchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
     }
 
     @Override
@@ -159,80 +163,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             long newRowId = db.insert(NoteContract.NoteEntry.TABLE_NAME, null, values);
         }
 
-//    private void displayDatabaseInfo() {
-//        // To access our database, we instantiate our subclass of SQLiteOpenHelper
-//        // and pass the context, which is the current activity.
-//        // Create and/or open a database to read from it
-//
-//        // Perform this raw SQL query "SELECT * FROM notes"
-//        // to get a Cursor that contains all rows from the pets table.
-//        //Cursor cursor = db.rawQuery("SELECT * FROM " + PetContract.PetEntry.TABLE_NAME, null);
-//
-//        TextView displayView = findViewById(R.id.displayView);
-//
-//        String[] projection = new String[]{
-//                NoteContract.NoteEntry._ID,
-//                NoteContract.NoteEntry.COLUMN_NOTE_TITLE,
-//                NoteContract.NoteEntry.COLUMN_NOTE_BODY,
-//                NoteContract.NoteEntry.COLUMN_NOTE_COLOR,
-//                NoteContract.NoteEntry.COLUMN_NOTE_TIME,
-//                };
-////      Cu
-//        Cursor cursor = getContentResolver().query(NoteContract.NoteEntry.CONTENT_URI,projection,null,null,null);
-//
-////        ListView petsListView = (ListView)findViewById(R.id.list);
-////
-////        PetCursorAdapter petAdapter =new PetCursorAdapter(this,cursor);
-////        petsListView.setAdapter(petAdapter);
-//
-//        try {
-//            // Display the number of rows in the Cursor (which reflects the number of rows in the
-//            // pets table in the database).
-//            // Create a header in the Text View that looks like this:
-//            //
-//            // The pets table contains <number of rows in Cursor> pets.
-//            // _id - name - breed - gender - weight
-//            //
-//            // In the while loop below, iterate through the rows of the cursor and display
-//            // the information from each column in this order.
-//            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-//            displayView.append(NoteContract.NoteEntry._ID + " - " +
-//                    NoteContract.NoteEntry.COLUMN_NOTE_TITLE + " - " +
-//                    NoteContract.NoteEntry.COLUMN_NOTE_BODY + " - " +
-//                    NoteContract.NoteEntry.COLUMN_NOTE_TIME + " - " +
-//                    NoteContract.NoteEntry.COLUMN_NOTE_COLOR + "\n");
-//
-////            // Figure out the index of each column
-//            int idColumnIndex = cursor.getColumnIndex(NoteContract.NoteEntry._ID);
-//            int titleColumnIndex = cursor.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_TITLE);
-//            int bodyColumnIndex = cursor.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_BODY);
-//            int timeColumnIndex = cursor.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_TIME);
-//            int colorColumnIndex = cursor.getColumnIndex(NoteContract.NoteEntry.COLUMN_NOTE_COLOR);
-////
-////            // Iterate through all the returned rows in the cursor
-//            while (cursor.moveToNext()) {
-//                // Use that index to extract the String or Int value of the word
-//                // at the current row the cursor is on.
-//                int currentID = cursor.getInt(idColumnIndex);
-//                String currentTitle = cursor.getString(titleColumnIndex);
-//                String currentBody = cursor.getString(bodyColumnIndex);
-//                int currentTime = cursor.getInt(timeColumnIndex);
-//                int currentColor = cursor.getInt(colorColumnIndex);
-//                displayView.append(("\n" + currentID + " - " +
-//                        currentTitle + " - " +
-//                        currentBody + " - " +
-//                        currentTime + " - " +
-//                        currentColor));
-//            }
-//        } finally {
-//            // Always close the cursor when you're done reading from it. This releases all its
-//            // resources and makes it invalid.
-//            cursor.close();
-//        }
-//    }
-
     @Override
     public Loader<Cursor>  onCreateLoader(int id, Bundle args) {
+
 
         String[] projection = new String[]{
                 NoteContract.NoteEntry._ID,
@@ -242,12 +175,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 NoteContract.NoteEntry.COLUMN_NOTE_TIME
         };
 
-        String constraint = "ч";
-        String selection = NoteContract.NoteEntry.COLUMN_NOTE_BODY + " LIKE '%"
-                + constraint + "%'"
-                + " OR " + NoteContract.NoteEntry.COLUMN_NOTE_TITLE + " LIKE '%"
-                + constraint + "%'"
-                ;
+        String selection = setupSelectionString();
 
         return new CursorLoader( this,
                 NoteContract.NoteEntry.CONTENT_URI,
@@ -255,6 +183,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 selection,
                 null,
                 NoteContract.NoteEntry.COLUMN_NOTE_COLOR);
+    }
+
+    private String setupSelectionString() {
+        String selection = null;
+        if (mSearchString != null) {
+            selection = NoteContract.NoteEntry.COLUMN_NOTE_BODY + " LIKE '%"
+                    + mSearchString + "%'"
+                    + " OR " + NoteContract.NoteEntry.COLUMN_NOTE_TITLE + " LIKE '%"
+                    + mSearchString + "%'";
+        }
+        return selection;
     }
 
 
